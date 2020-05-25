c7ffe1da1855376c34d24c9fe5898f5de5ad9c3b
Gailbert <gailbert.huang@gmail.com>
2020-05-25 11:00:21 +0800

    Improvement on Lobby Scene

--- Assets/Resources/Script/Lobby/LobbyManager.cs
+++ Assets/Resources/Script/Lobby/LobbyManager.cs
@@ -4,9 +4,27 @@ using UnityEngine;
 
 public class LobbyManager : MonoBehaviour
 {
-	[SerializeField] GameObject genericPopup;
+    //Turn into a list someday
+    [SerializeField] private GameObject HomeContent;
+    [SerializeField] private GameObject UpgradeContent;
+    [SerializeField] private GameObject TeamContent;
+    [SerializeField] private GameObject ShopContent;
+    [SerializeField] private GameObject SettingContent; //Not official just place holder
+    [SerializeField] private LobbyOptionHandler OptionHandler;
 
-	public void PlayButtonClick()
+    [SerializeField] GameObject genericPopup;
+    private GameObject currentContentShowing = null;
+
+    private static LobbyManager instance;
+    public static LobbyManager Instance { get { return instance; } }
+
+    void Awake()
+    {
+        instance = this;
+        OptionHandler.Initialize();
+    }
+
+    public void PlayButtonClick()
 	{
         if(LoadingManager.Instance != null)
         {
@@ -19,4 +37,47 @@ public class LobbyManager : MonoBehaviour
             Debug.LogError("Loading Manager is missing");
         }
 	}
+
+    private void HideCurrentContent()
+    {
+        if (currentContentShowing != null)
+        {
+            currentContentShowing.SetActive(false);
+        }
+    }
+
+    public void ShowHomeContent()
+    {
+        HideCurrentContent();
+        currentContentShowing = HomeContent;
+        currentContentShowing.SetActive(true);
+    }
+
+    public void ShowUpgradeContent()
+    {
+        HideCurrentContent();
+        currentContentShowing = UpgradeContent;
+        currentContentShowing.SetActive(true);
+    }
+
+    public void ShowTeamContent()
+    {
+        HideCurrentContent();
+        currentContentShowing = TeamContent;
+        currentContentShowing.SetActive(true);
+    }
+
+    public void ShowShopContent()
+    {
+        HideCurrentContent();
+        currentContentShowing = ShopContent;
+        currentContentShowing.SetActive(true);
+    }
+
+    public void ShowSettingContent()
+    {
+        HideCurrentContent();
+        currentContentShowing = SettingContent;
+        currentContentShowing.SetActive(true);
+    }
 }
---
+++ Assets/Resources/Script/Lobby/LobbyOptionHandler.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyOptionHandler : MonoBehaviour
+{
+    [SerializeField] private Button HomeButton;
+    [SerializeField] private Button UpgradeButton;
+    [SerializeField] private Button TeamButton;
+    [SerializeField] private Button ShopButton;
+    [SerializeField] private Button SettingButton;
+    private Button currentActiveButton;
+
+    public void Initialize()
+    {
+        OnPressHomeButton();
+    }
+
+    public void OnPressHomeButton()
+    {
+        HomeButton.interactable = false;
+        ReactivateCurrentActiveButton();
+        currentActiveButton = HomeButton;
+        LobbyManager.Instance?.ShowHomeContent();
+    }
+
+    public void OnPressUpgradeButtonButton()
+    {
+        UpgradeButton.interactable = false;
+        ReactivateCurrentActiveButton();
+        currentActiveButton = UpgradeButton;
+        LobbyManager.Instance?.ShowUpgradeContent();
+    }
+
+    public void OnPressTeamButton()
+    {
+        TeamButton.interactable = false;
+        ReactivateCurrentActiveButton();
+        currentActiveButton = TeamButton;
+        LobbyManager.Instance?.ShowTeamContent();
+    }
+
+    public void OnPressShopButton()
+    {
+        ShopButton.interactable = false;
+        ReactivateCurrentActiveButton();
+        currentActiveButton = ShopButton;
+        LobbyManager.Instance?.ShowShopContent();
+    }
+
+    public void OnPressSettingButton()
+    {
+        SettingButton.interactable = false;
+        ReactivateCurrentActiveButton();
+        currentActiveButton = SettingButton;
+        LobbyManager.Instance?.ShowSettingContent();
+    }
+
+    private void ReactivateCurrentActiveButton()
+    {
+        if (currentActiveButton != null)
+        {
+            currentActiveButton.interactable = true;
+        }
+    }
+}
